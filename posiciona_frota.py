ffb02080b85dd36c226f8591f446d39aeca28cd2
joaovcalmeida <joao.v.campos.almeida@gmail.com>
2023-05-03 14:30:45 -0300

    posiciona frota feito

---
+++ posiciona_frota.py
@@ -0,0 +1,7 @@
+def posiciona_frota(dicionario):
+    tabuleiro = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+    for posicoes in dicionario.values():
+        for navio in posicoes:
+            for posicao in navio:
+                tabuleiro[posicao[0]][posicao[1]] = 1
+    return tabuleiro
